9b0869490ef14e18b8173c370fb4148501a070e9
Francisco <francisco@standard.ai>
2020-11-19 15:04:06 +0900

    Added comment. A bit more legible as well

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,8 @@
 import regeneratorRuntime from "regenerator-runtime";
 import crypto from "crypto";
 
-Object.defineProperty(global.self, "crypto", {
-  value: {
-    getRandomValues: arr => crypto.randomBytes(arr.length)
-  }
-});
+// https://stackoverflow.com/q/52612122/12271991
+const getRandomValues = arr => crypto.randomBytes(arr.length);
+Object.defineProperty(global.self, "crypto", { value: { getRandomValues } });
 
 global.crypto.subtle = {};
